db4508186f6e2d6e2d21e1c62523eaa3d316b00c
mikiyas <mikiyasget1234@gmail.com>
2023-07-14 16:37:01 +0100

    modify main-nav

--- app/components/main-nav.tsx
+++ app/components/main-nav.tsx
@@ -4,6 +4,7 @@ import { usePathname } from "next/navigation";
 
 import { Category } from "@/types";
 import Link from "next/link";
+import { cn } from "@/libs/utils";
 
 interface MainNavProps {
   data: Category[];
@@ -19,9 +20,16 @@ const MainNav: React.FC<MainNavProps> = ({ data }) => {
   }));
 
   return (
-    <nav>
+    <nav className="mx-6 flex items-center space-x-4 lg:space-x-6">
       {routes?.map((route) => (
-        <Link key={route.href} href={route.href} className="">
+        <Link
+          key={route.href}
+          href={route.href}
+          className={cn(
+            "text-sm font-medium hover:text-black transition-colors",
+            route.active ? "text-black" : "text-neutral-500"
+          )}
+        >
           {route.label}
         </Link>
       ))}
