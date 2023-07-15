f705a021b3027a4881a1c1030b17259eae983e56
mikiyas <mikiyasget1234@gmail.com>
2023-07-15 16:07:32 +0100

    product info component

---
+++ app/components/info.tsx
@@ -0,0 +1,42 @@
+import { Product } from "@/types";
+import Currency from "./ui/currency";
+import Button from "./ui/button";
+import { ShoppingCart } from "lucide-react";
+interface Infoprops {
+  data: Product;
+}
+
+const Info: React.FC<Infoprops> = ({ data }) => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold text-gray-900">{data.name}</h1>
+      <div className="mt-3 flex items-end justify-between">
+        <p className="text-2xl text-gray-900">
+          <Currency value={data?.price} />
+        </p>
+      </div>
+      <hr className="my-4" />
+      <div className="flex flex-col gap-y-6">
+        <div className="flex items-center gap-x-4">
+          <h3 className="font-semibold text-black">Size:</h3>
+          <div className="">{data?.size?.value}</div>
+        </div>
+        <div className="flex items-center gap-x-4">
+          <h3 className="font-semibold text-black">Color:</h3>
+          <div
+            className="h-6 w-6 rounded-full border border-gray-600"
+            style={{ backgroundColor: data?.color?.value }}
+          />
+        </div>
+        <div className="mt-10">
+          <Button className="flex items-center gap-x-2">
+            Add to cart
+            <ShoppingCart size={20} />
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Info;
