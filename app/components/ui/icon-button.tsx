aa59c5f98cb1e8b6088aeda8741a4ec942d46ee0
mikiyas <mikiyasget1234@gmail.com>
2023-07-24 07:42:11 +0100

    add an icon button

--- app/components/ui/icon-button.tsx
+++ app/components/ui/icon-button.tsx
@@ -13,6 +13,7 @@ const IconButton: React.FC<IconButtonProps> = ({
 }) => {
   return (
     <button
+      onClick={onClick}
       className={cn(
         "rounded-full flex items-center justify-center bg-white border shadow-md p-2 hover:scale-110 transition",
         className
