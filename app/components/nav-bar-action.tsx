b67ba88e663a94c43f0180813b39afc58f677630
mikiyas <mikiyasget1234@gmail.com>
2023-07-24 07:41:03 +0100

    nav-bar

--- app/components/nav-bar-action.tsx
+++ app/components/nav-bar-action.tsx
@@ -3,9 +3,13 @@
 import { ShoppingBag } from "lucide-react";
 import Button from "./ui/button";
 import { useEffect, useState } from "react";
+import useCart from "@/hooks/use-cart";
+import { useRouter } from "next/navigation";
 
 const NavbarAction = () => {
   const [mounted, setMounted] = useState(false);
+  const cart = useCart();
+  const router = useRouter();
 
   useEffect(() => {
     setMounted(true);
@@ -14,11 +18,17 @@ const NavbarAction = () => {
   if (!mounted) {
     return null;
   }
+
   return (
     <div className="ml-auto flex items-center gap-x-2">
-      <Button className="flex items-center bg-black px-4 py-2 gap-x-2">
+      <Button
+        onClick={() => router.push("/cart")}
+        className="flex items-center bg-black px-4 py-2 gap-x-2"
+      >
         <ShoppingBag size={20} color="white" />
-        <span className="text-sm font-medium ml-auto text-white">0</span>
+        <span className="text-sm font-medium ml-auto text-white">
+          {cart.items.length}
+        </span>
       </Button>
     </div>
   );
