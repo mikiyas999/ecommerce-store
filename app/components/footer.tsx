a9072b0e6f3b372127a376c372f9137a546c2293
Mikiyas Getachhew <125805964+mikiyas999@users.noreply.github.com>
2024-03-06 05:54:31 +0300

    Update footer.tsx

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -3,7 +3,7 @@ const Footer = () => {
     <footer className="bg-white border-t">
       <div className="mx-auto py-10">
         <p className="text-center text-sm text-black">
-          &copy; 2023 Store, Inc. All rights reserved.
+          &copy; 2024 Store, Inc. All rights reserved.
         </p>
       </div>
     </footer>
